f718fa96b20b212b77bd566c79434bf05a0d997a
humbertotm <humberto.htm@gmail.com>
2019-12-01 15:14:45 -0700

    Db query and insert operations added

--- src/db/operations.clj
+++ src/db/operations.clj
@@ -2,13 +2,18 @@
   (:require [clojure.java.jdbc :as jdbc])
   (:require [db.core :as core]))
 
+;; These are simplistic wrappers over jdbc native functions. Will revisit this as progress
+;; is made in the project and use cases become more apparent.
+
 (defn query
-  "Queries the db with the given SQL string and parameterized values"
-  [sql-string & vals]
-  ())
+  "Queries the db with the given SQL string and parameterized values
+   Follows the same API as jdbc/query when it comes to the query string and parameterized
+   values"
+  [sql-query]
+  (jdbc/query (core/connection) sql-query))
 
 (defn insert
   "Inserts the given record into the specified table"
-  [connection table record]
-  (jdbc/insert! connection table record))
+  [table record]
+  (jdbc/insert! (core/connection) table record))
 
