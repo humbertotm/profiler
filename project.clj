ff9e32c6b1c469c896ce97fa9e073baeb5a931d7
humbertotm <humberto.htm@gmail.com>
2020-02-22 20:46:19 -0700

    Refactored loader and changed name to etl. It works now

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/java.jdbc "0.7.10"]
                  [org.postgresql/postgresql "42.2.8"]
                  [com.mchange/c3p0 "0.9.5.4"]
-                 [org.clojure/data.csv "0.1.4"]]
+                 [org.clojure/data.csv "0.1.4"]
+                 [clojure.java-time "0.3.2"]]
   :main ^:skip-aot screener.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/screener/models/core.clj
+++ src/screener/models/core.clj
@@ -1,5 +1,7 @@
 (ns screener.models.core
   (:require [clojure.spec.alpha :as s]
+            [java-time :as jtime]
+            [clojure.string :as st]
             [screener.models.num :refer :all]
             [screener.models.tag :refer :all]
             [screener.models.pre :refer :all]
@@ -9,15 +11,20 @@
            [screener.models.pre Pre]
            [screener.models.sub Sub]))
 
-(def records-map {:sub "Sub"
-                  :tag "Tag"
-                  :num "Num"
-                  :pre "Pre"})
+(def records-map {:sub "sub"
+                  :tag "tag"
+                  :num "num"
+                  :pre "pre"})
 
 (def tables {:sub :submissions
              :tag :tags
              :num :numbers
              :pre :presentations})
+
+(defmacro create-record
+  "Returns expression that will create a record from specified record-type and src-map"
+  [record-type src-map]
+  `((symbol (str (~records-map ~record-type))) ~src-map))
 
 ;; Field validations
 
@@ -50,7 +57,6 @@
 (s/def ::countryma (s/and string? #(<= (count %) 2)))
 
 (s/def ::stprma (s/and string? #(<= (count %) 2)))
-
 (s/def ::cityma (s/and string? #(<= (count %) 30)))
 
 (s/def ::zipma (s/and string? #(<= (count %) 10)))
@@ -77,20 +83,32 @@
 
 (s/def ::form (s/and string? #(<= (count %) 10)))
 
-;; [wololo] A regex for yymmdd date format is necessary for this validation
-(s/def ::period (s/and string? #(<= (count %) 8)))
+;; Could be better specified with a DateTimeParseException
+(s/def ::period (s/and string?
+                       #(<= (count %) 8)
+                       #(try (jtime/local-date "yyyyMMdd" %)
+                             (catch Exception e
+                                 false))))
 
 (s/def ::fy (s/and string? #(<= (count %) 4)))
 
 (s/def ::fp (s/and string? #(<= (count %) 2)))
 
-;; [wololo] same regex as for ::period
-(s/def ::filed (s/and string? #(<= (count %) 8)))
+(s/def ::filed (s/and string?
+                      #(<= (count %) 8)
+                      #(try (jtime/local-date "yyyyMMdd" %)
+                            (catch Exception e
+                              false))))
 
-;; [wololo] regex for yyyy-mm-dd hh::mm::ss
-(s/def ::accepted (s/and string? #(<= (count %) 19)))
+(s/def ::accepted (s/and string?
+                         #(<= (count %) 24)
+                         #(try (jtime/local-date-time "yyyy-MM-dd HH:mm:ss.S" %)
+                               (catch Exception e
+                                 false))))
 
 (s/def ::prevrpt #(number? (read-string %)))
+
+(s/def ::coreg (s/and string? #(<= (count %) 256)))
 
 (s/def ::detail #(number? (read-string %)))
 
@@ -110,16 +128,17 @@
 
 (s/def ::doc (s/and string? #(<= (count %) 2048)))
 
-(s/def ::coreg #(number? (read-string %)))
-
-;; [wololo] regex yyyymmdd
-(s/def ::ddate (s/and string? #(<= (count %) 8)))
+(s/def ::ddate (s/and string?
+                      #(<= (count %) 8)
+                      #(try (jtime/local-date "yyyyMMdd" %)
+                            (catch Exception e
+                              false))))
 
 (s/def ::qtrs #(number? (read-string %)))
 
 (s/def ::uom (s/and string? #(<= (count %) 20)))
 
-(s/def ::value #(number? (read-string %)))
+(s/def ::value (s/and string? #(not (st/blank? %)) #(number? (read-string %))))
 
 (s/def ::footnote string?)
 
@@ -160,6 +179,40 @@
 ;; TODO: add an intermediate transformation layer between csv data and data passed to
 ;; constructors
 
+;; TODO: Fix booleans. Failing upon db write.
+
+(defn string-or-nil
+  "Returns lambda that returns a string value if present or nil"
+  []
+  #(if (not (empty? %)) % nil))
+
+(defn number-or-nil
+  "Returns lambda that returns a number value from string if present or nil"
+  []
+  #(if (not (empty? %))
+     (try (read-string %)
+          (catch Exception e
+            (do (println (str "Invalid number: " %))
+                nil)))
+     nil))
+
+(defn date-or-nil
+  "Returns lambda that returns date value from string if present or nil"
+  []
+  #(if (not (empty? %1)) (jtime/local-date %2 %1) nil))
+
+(defn datetime-or-nil
+  "Returns lambda that returns datetime value from string if present or nil"
+  []
+  #(if (not (empty? %1)) (jtime/local-date-time %2 %1) nil))
+
+(defn boolean-or-nil
+  "Returns a lambda that returns a boolean value from a string being either 1 or 0"
+  []
+  #(if (not (empty? %))
+     (if (= (read-string %) 1) true false)
+     nil))
+
 (defn create-num
   "Creates a new Num record from a map with all-string values from csv"
   [num]
@@ -173,15 +226,15 @@
                 uom
                 value
                 footnote]} num]
-    (->Num adsh
-           tag
-           version
-           (read-string coreg)
-           ddate
-           (read-string qtrs)
-           uom
-           (read-string value)
-           footnote)))
+    (->Num ((string-or-nil) adsh)
+           ((string-or-nil) tag)
+           ((string-or-nil) version)
+           ((string-or-nil) coreg)
+           ((date-or-nil) ddate "yyyyMMdd")
+           ((number-or-nil) qtrs)
+           ((string-or-nil) uom)
+           ((number-or-nil) value)
+           ((string-or-nil) footnote))))
 
 (defn create-pre
   [pre]
@@ -195,15 +248,15 @@
                 tag
                 version
                 plabel]} pre]
-    (->Pre adsh
-           (read-string report)
-           (read-string line)
-           stmt
-           (read-string inpth)
-           rfile
-           tag
-           version
-           plabel)))
+    (->Pre ((string-or-nil) adsh)
+           ((string-or-nil) report)
+           ((number-or-nil) line)
+           ((string-or-nil) stmt)
+           ((boolean-or-nil) inpth)
+           ((string-or-nil) rfile)
+           ((string-or-nil) tag)
+           ((string-or-nil) version)
+           ((string-or-nil) plabel))))
 
 (defn create-tag
   [tag]
@@ -217,15 +270,15 @@
                 crdr
                 tlabel
                 doc]} tag]
-    (->Tag tag
-           version
-           (read-string custom)
-           (read-string abstract)
-           datatype
-           iord
-           crdr
-           tlabel
-           doc)))
+    (->Tag ((string-or-nil) tag)
+           ((string-or-nil) version)
+           ((boolean-or-nil) custom)
+           ((boolean-or-nil) abstract)
+           ((string-or-nil) datatype)
+           ((string-or-nil) iord)
+           ((string-or-nil) crdr)
+           ((string-or-nil) tlabel)
+           ((string-or-nil) doc))))
 
 (defn create-sub
   [sub]
@@ -266,40 +319,40 @@
                 instance
                 nciks
                 aciks]} sub]
-    (->Sub adsh
-           (read-string cik)
-           name
-           (read-string sic)
-           countryba
-           stprba
-           cityba
-           zipba
-           bas1
-           bas2
-           baph
-           countryma
-           stprma
-           cityma
-           zipma
-           mas1
-           mas2
-           countryinc
-           stprinc
-           (read-string ein)
-           former
-           changed
-           afs
-           (read-string wksi)
-           fye
-           form
-           period
-           fy
-           fp
-           filed
-           accepted
-           (read-string prevrpt)
-           (read-string detail)
-           instance
-           (read-string nciks)
-           aciks)))
+    (->Sub ((string-or-nil) adsh)
+           ((string-or-nil) cik)
+           ((string-or-nil) name)
+           ((string-or-nil) sic)
+           ((string-or-nil) countryba)
+           ((string-or-nil) stprba)
+           ((string-or-nil) cityba)
+           ((string-or-nil) zipba)
+           ((string-or-nil) bas1)
+           ((string-or-nil) bas2)
+           ((string-or-nil) baph)
+           ((string-or-nil) countryma)
+           ((string-or-nil) stprma)
+           ((string-or-nil) cityma)
+           ((string-or-nil) zipma)
+           ((string-or-nil) mas1)
+           ((string-or-nil) mas2)
+           ((string-or-nil) countryinc)
+           ((string-or-nil) stprinc)
+           ((string-or-nil) ein)
+           ((string-or-nil) former)
+           ((date-or-nil) changed "yyyyMMdd")
+           ((string-or-nil) afs)
+           ((boolean-or-nil) wksi)
+           ((string-or-nil) fye)
+           ((string-or-nil) form)
+           ((date-or-nil) period "yyyyMMdd")
+           ((string-or-nil) fy)
+           ((string-or-nil) fp)
+           ((date-or-nil) filed "yyyyMMdd")
+           ((datetime-or-nil) accepted "yyyy-MM-dd HH:mm:ss.S")
+           ((boolean-or-nil) prevrpt)
+           ((boolean-or-nil) detail)
+           ((string-or-nil) instance)
+           ((string-or-nil) nciks)
+           ((string-or-nil) aciks))))
 
