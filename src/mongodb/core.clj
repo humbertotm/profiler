8bd551b8920af0edf818992312bd1abfaff194e6
humbertotm <humberto.htm@gmail.com>
2020-09-18 21:36:12 -0700

    Fixed mongodb connection handling

--- src/mongodb/core.clj
+++ src/mongodb/core.clj
@@ -5,11 +5,17 @@
             [monger.credentials :as mgcreds]
             [monger.collection :as mgcoll]))
 
+(def mongoconn
+  "Stores the single mongodb connection employed"
+  (atom nil))
+
 (def host-data
+  "Reads mongodb host data from environment"
   {:ipaddr (env :mongo-host-ipaddr)
    :port (Integer. (env :mongo-host-port))})
 
 (def user-auth-data
+  "Reads mongodb user from environment"
   {:auth-db-name (env :mongo-auth-db)
    :username (env :mongo-username)
    :password (env :mongo-password)})
@@ -17,22 +23,37 @@
 (def profiler-db-name "profiler")
 
 (def creds
+  "Returns a set of creds to be employed in authenticated mongodb connection"
   (mgcreds/create
    (:username user-auth-data)
    (:auth-db-name user-auth-data)
    (.toCharArray (:password user-auth-data))))
 
-(def mongoconn
+(defn initialize-mongoconn
+  "Initializes a new mongodb connection"
+  []
   (mg/connect-with-credentials
    (:ipaddr host-data)
    (:port host-data)
    creds))
 
-(defn get-profiler-db
+(defn get-mongoconn
+  "Returns existing connection. If none, creates and returns a new one"
   []
-  (mg/get-db mongoconn profiler-db-name))
+  (if (nil? @mongoconn)
+    (swap! mongoconn (fn [current-conn] (initialize-mongoconn)))
+    @mongoconn))
 
-(defn disconnect
-  [conn]
-  (mg/disconnect conn))
+(defn get-profiler-db
+  "Returns db to be employed for profiler purposes"
+  []
+  (mg/get-db (get-mongoconn) profiler-db-name))
+
+(defn disconnect-mongoconn
+  "Disconnects existing mongoconn"
+  []
+  (if (not (nil? @mongoconn))
+    (do (mg/disconnect @mongoconn)
+        (swap! mongoconn (fn [current-conn] nil)))
+    nil))
 
