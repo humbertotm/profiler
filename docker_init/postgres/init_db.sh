feac3dea30e3c4d532f98c45cbc70bc5c511be4d
humbertotm <humberto.htm@gmail.com>
2020-09-27 21:08:58 -0700

    Added comments to init sql script

--- docker_init/postgres/init_db.sh
+++ docker_init/postgres/init_db.sh
@@ -3,6 +3,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    -- Clean up existing template1 and restore it to suitable settings
     UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template0';
     UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
     DROP DATABASE template1;
@@ -10,8 +11,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';
     UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template0';
 
+    -- Create app database
     CREATE DATABASE screener_dev;
     
+    -- Create app database user
     CREATE USER screeneruser;
     ALTER USER screeneruser WITH PASSWORD 'screeneruser';
     GRANT ALL PRIVILEGES ON DATABASE screener_dev TO screeneruser;
