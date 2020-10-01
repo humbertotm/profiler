824a502d95f5918437c7a590f506aca4733c606b
humbertotm <humberto.htm@gmail.com>
2020-09-30 19:34:23 -0700

    Added compose file, initialization files for db. Will move them later to a more suitable place

--- docker_init/mongo/init.js
+++ docker_init/mongo/init.js
@@ -1,5 +1,7 @@
 // This stays here while we test out this mongo docker container init
-
+// Was not executed during compose initialization. Need to determine if this is needed
+// at all or if first inserts and db selection from lein app are enough to initialize db
+// and collection.
 db = db.getSiblingDB('profiler')
 db.profiles.insert(
     {
--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
-{:dev {:env {:db-subname "//localhost:5432/screener_dev"
+{:dev {:env {:db-subname "//screener-postgres:5432/screener_dev"
              :db-user "screeneruser"
              :db-password "screeneruser"
-             :mongo-host-ipaddr "screenermdb" ; This matches the network alias for mongodb container
+             :mongo-host-ipaddr "screener-mongo"
              :mongo-host-port 27017
              :mongo-auth-db "admin"
              :mongo-username "mongoadmin"
